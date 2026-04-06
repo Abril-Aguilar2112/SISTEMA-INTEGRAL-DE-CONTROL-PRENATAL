cdfcf6e729e3a175a7db7a2f7d01de32f7ec0214
Abril-Aguilar2112 <17241110790@utectulancingo.edu.mx>
2026-04-06 10:06:56 -0600

    refactor: mejora de manejo de errores y logica de inicio de sesion y registro en el cliente de supabase auth_service.py

--- app/web/services/auth_service.py
+++ app/web/services/auth_service.py
@@ -1,36 +1,83 @@
 from app.web.utils.supabase_client import supabase
 
 def register_user(email, password, nombre, rol):
-    auth_response = supabase.auth.sign_up({
-        "email": email,
-        "password": password
-    })
+    try:
+        existing = supabase.table("usuario").select().eq("correo", email).execute()
 
-    if auth_response.user is None:
-        return {"error": "No se pudo registrar"}
+        if existing.data:
+            return {"data": {}, "message": "Ocurrio un error", "error": "El correo ya está registrado"}
 
-    user_data = {
-        "auth_id": auth_response.user.id,
-        "correo": email,
-        "nombre": nombre,
-        "rol": rol
-    }
+        auth_response = supabase.auth.sign_up({
+            "email": email,
+            "password": password
+        })
 
-    supabase.table("usuario").insert(user_data).execute()
+        if auth_response.user is None:
+            return {"data": {}, "message": "Ocurrio un error", "error": "No se pudo registrar"}
 
-    return {"message": "Usuario registrado"}
+        
+        user_data = {
+            "auth_id": auth_response.user.id,
+            "correo": email,
+            "nombre": nombre,
+            "rol": rol
+        }
+
+        db_response = supabase.table("usuario").insert(user_data).execute()
+
+        if not db_response.data:
+            return {"data": {}, "message": "Ocurrio un error", "error": "No se pudo registrar el usuario"}
+
+        usuario_db = db_response.data
+
+        return {
+            "data": {
+                "id_usuario": usuario_db["id_usuario"],
+                "nombre": usuario_db["nombre"],
+                "correo": usuario_db["correo"],
+                "rol": usuario_db["rol"],
+                "access_token": auth_response.session.access_token
+            },
+            "message": "Usuario registrado exitosamente",
+            "error": None
+        }
+
+    except Exception as e:
+        return {"data": {}, "message": "Ocurrio un error", "error": str(e)}
 
 
 def login_user(email, password):
-    response = supabase.auth.sign_in_with_password({
-        "email": email,
-        "password": password
-    })
+    try:
+        auth_response = supabase.auth.sign_in_with_password({
+            "email": email,
+            "password": password
+        })
 
-    if response.user is None:
-        return {"error": "Credenciales inválidas"}
+        if not auth_response.user:
+            return {"data": {}, "message": "Ocurrio un error", "error": "Credenciales inválidas"}
 
-    return {
-        "access_token": response.session.access_token,
-        "user": response.user
-    }
+        user = auth_response.user
+
+        db_response = supabase.table("usuario").select().eq("auth_id", user.id).single().execute()
+
+        if not db_response.data:
+            return {"data": {}, "message": "Ocurrio un error", "error": "Usuario no registrado en el sistema"}
+
+        if not db_response:
+            return {"data": {}, "message": "Ocurrio un error", "error": "No se encontró el usuario"}
+
+        usuario_db = db_response.data
+
+        return {
+            "data": {
+                "id_usuario": usuario_db["id_usuario"],
+                "nombre": usuario_db["nombre"],
+                "correo": usuario_db["correo"],
+                "rol": usuario_db["rol"],
+                "access_token": auth_response.session.access_token
+            },
+            "message": "Inicio de sesión exitoso",
+            "error": None
+        }
+    except Exception as e:
+        return {"data": {}, "message": "Ocurrio un error", "error": str(e)}
